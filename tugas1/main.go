436c71333462338b265e6ab04f6992dfc451d8b9
miftahers <miftahers@gmail.com>
2022-08-23 11:49:55 +0700

    adding 'make triangle' feature

--- tugas1/main.go
+++ tugas1/main.go
@@ -7,4 +7,12 @@ func main() {
 	fmt.Println("seluruh dunia")
 	// stash
 	fmt.Println("Dan Dunia")
+
+	// Menambahkan looping membuat segitiga siku-siku
+	for i := 1; i < 10; i++ {
+		for j := 1; j < i; j++ {
+			fmt.Print("#")
+		}
+		fmt.Println()
+	}
 }
