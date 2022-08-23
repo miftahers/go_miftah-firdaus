5223c4c0a36cb811f08ed6a3e345cf0e0cb2d138
miftahers <miftahers@gmail.com>
2022-08-23 11:58:31 +0700

    adding multiplication feature

--- tugas1/main.go
+++ tugas1/main.go
@@ -7,4 +7,14 @@ func main() {
 	fmt.Println("seluruh dunia")
 	// stash
 	fmt.Println("Dan Dunia")
+
+	// Membuat fitur perkalian dari input
+	var a1 int
+	var a2 int
+	fmt.Println("========= Perkalian ==========")
+	fmt.Println("masukan angka pertama: ")
+	fmt.Scanln(&a1)
+	fmt.Println("masukan angka kedua: ")
+	fmt.Scanln(&a2)
+	fmt.Println("Hasilnya adalah", a1*a2)
 }
