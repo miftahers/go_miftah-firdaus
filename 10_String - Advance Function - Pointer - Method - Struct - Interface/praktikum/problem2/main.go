452f4d268c8b2fc0cc752df2ed3929be9366a2c2
= <=>
2023-09-12 16:47:21 +0700

    update caesar cipher

--- 10_String - Advance Function - Pointer - Method - Struct - Interface/praktikum/problem2/main.go
+++ 10_String - Advance Function - Pointer - Method - Struct - Interface/praktikum/problem2/main.go
@@ -11,20 +11,25 @@ import (
 )
 
 func main() {
-	fmt.Println(Caesar(3, "abc"))                           // def
-	fmt.Println(Caesar(2, "alta"))                          // cnvc
-	fmt.Println(Caesar(10, "alterraacademy"))               // kvdobbkkmknowi
-	fmt.Println(Caesar(1, "abcdefghijklmnopqrstuvwxyz"))    // bcdefghijklmnopqrstuvwxyza
-	fmt.Println(Caesar(1000, "abcdefghijklmnopqrstuvwxyz")) // mnopqrstuvwxyzabcdefghijkl
+	fmt.Println(CaesarIgnoreSpace(3, "abc"))                           // def
+	fmt.Println(CaesarIgnoreSpace(2, "alta"))                          // cnvc
+	fmt.Println(CaesarIgnoreSpace(10, "alterraacademy"))               // kvdobbkkmknowi
+	fmt.Println(CaesarIgnoreSpace(1, "abcdefghijklmnopqrstuvwxyz"))    // bcdefghijklmnopqrstuvwxyza
+	fmt.Println(CaesarIgnoreSpace(1000, "abcdefghijklmnopqrstuvwxyz")) // mnopqrstuvwxyzabcdefghijkl
+	fmt.Println(CaesarCipherRefactored(7, "afila ansori"))
+	fmt.Println((CaesarCipher(-7, "hmpsh huzvyp")))
 }
 
-func Caesar(offset int, input string) string {
+func CaesarIgnoreSpace(offset int, input string) string {
 
 	str := strings.ToLower(input)
 	tmp := offset % 26
 	var result []string
 
-	for i, _ := range str {
+	for i := range str {
+		if str[i] == byte(' ') {
+			continue
+		}
 		if str[i]+byte(tmp) <= 122 {
 			s := string((str[i]) + byte(tmp))
 			result = append(result, s)
@@ -36,4 +41,62 @@ func Caesar(offset int, input string) string {
 		}
 	}
 	return strings.Join(result, "")
+}
+
+func CaesarCipherRefactored(shift int, input string) string {
+	inputLowerCase := strings.ToLower(input)
+	shiftAmount := shift % 26
+	var encryptedChars []string
+
+	for i := range inputLowerCase {
+		char := inputLowerCase[i]
+
+		if char+byte(shiftAmount) <= 'z' {
+			shiftedChar := string(char + byte(shiftAmount))
+			encryptedChars = append(encryptedChars, shiftedChar)
+		} else {
+			remainingShift := 'z' - char
+			wrappedShift := byte(shiftAmount) - remainingShift
+			wrappedChar := string('a' + wrappedShift - 1)
+			encryptedChars = append(encryptedChars, wrappedChar)
+		}
+	}
+
+	return strings.Join(encryptedChars, "")
+}
+
+func CaesarCipher(shift int, input string) string {
+	shiftAmount := shift % 26
+	var encryptedChars []string
+
+	for i := range input {
+		char := input[i]
+
+		if 'a' <= char && char <= 'z' {
+			if char+byte(shiftAmount) <= 'z' {
+				shiftedChar := string(char + byte(shiftAmount))
+				encryptedChars = append(encryptedChars, shiftedChar)
+			} else {
+				remainingShift := 'z' - char
+				wrappedShift := byte(shiftAmount) - remainingShift
+				wrappedChar := string('a' + wrappedShift - 1)
+				encryptedChars = append(encryptedChars, wrappedChar)
+			}
+		} else if 'A' <= char && char <= 'Z' {
+			if char+byte(shiftAmount) <= 'Z' {
+				shiftedChar := string(char + byte(shiftAmount))
+				encryptedChars = append(encryptedChars, shiftedChar)
+			} else {
+				remainingShift := 'Z' - char
+				wrappedShift := byte(shiftAmount) - remainingShift
+				wrappedChar := string('A' + wrappedShift - 1)
+				encryptedChars = append(encryptedChars, wrappedChar)
+			}
+		} else {
+			// Preserve non-alphabet characters
+			encryptedChars = append(encryptedChars, string(char))
+		}
+	}
+
+	return strings.Join(encryptedChars, "")
 }
